38bb8c5a3ec4ae26d484cfca6df003d9fa275a73
LanaZahrEddin <43419131+LanaZahrEddin@users.noreply.github.com>
2019-09-30 13:08:54 -0400

    added Guess Number

---
+++ GuessNumberLanaZE/GuessNumberLanaZE/GuessNumberForm.cs
@@ -0,0 +1,34 @@
+﻿/*
+     * Created by: Lana ZE
+     * Created on: 30-09-2019
+     * Created for: ICS3U Programming
+     * Daily Assignment – Day #16 - Guess Number
+     * This program asks the user to guess a number between 1 and 10.
+     * If they get it right, a question mark is displayed, and a correct sound is displayed.
+     * If they get it wrong, an X is displayed and an incorrect sound is displayed. The number that is generated in random.
+    */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumberLanaZE
+{
+    public partial class frmGuessNumber : Form
+    {
+        public frmGuessNumber()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmGuessNumber_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
