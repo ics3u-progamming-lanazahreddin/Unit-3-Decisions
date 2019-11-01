1614509768b6bc7dbd6b92f08a47eb769db63ce6
LanaZahrEddin <43419131+LanaZahrEddin@users.noreply.github.com>
2019-11-01 12:43:48 -0400

    added number game

---
+++ NumberGame/NumberGame/NumberGameLanaZEForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace NumberGame
+{
+    partial class frmNumberGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQ1 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.lblA2 = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblQ1
+            // 
+            this.lblQ1.AutoSize = true;
+            this.lblQ1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQ1.Location = new System.Drawing.Point(12, 92);
+            this.lblQ1.Name = "lblQ1";
+            this.lblQ1.Size = new System.Drawing.Size(444, 33);
+            this.lblQ1.TabIndex = 1;
+            this.lblQ1.Text = "Guess and integer between 1-10:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(504, 105);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(97, 20);
+            this.txtA.TabIndex = 2;
+            // 
+            // lblA2
+            // 
+            this.lblA2.AutoSize = true;
+            this.lblA2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA2.Location = new System.Drawing.Point(277, 307);
+            this.lblA2.Name = "lblA2";
+            this.lblA2.Size = new System.Drawing.Size(47, 33);
+            this.lblA2.TabIndex = 3;
+            this.lblA2.Text = "??";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCheck.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCheck.Location = new System.Drawing.Point(301, 204);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(155, 53);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // frmNumberGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblA2);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblQ1);
+            this.Name = "frmNumberGame";
+            this.Text = "Numbe Game by Lana ZE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblQ1;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label lblA2;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ NumberGame/NumberGame/NumberGameLanaZEForm.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by: Lana ZE
+ * Created on: 1-november-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #17 - Number Game
+ * This program asks the user to guess a randomly generated number
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace NumberGame
+{
+    public partial class frmNumberGame : Form
+    {
+        //declare global variables and constants
+        int randomNumber;
+        Random numberGenerator = new Random();
+        const int MIN_VALUE = 1;
+        const int MAX_VALUE = 10;
+
+        public frmNumberGame()
+        {
+            InitializeComponent();
+            randomNumber = numberGenerator.Next(MIN_VALUE, MAX_VALUE + 1);
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            //turns the user's guess into a value
+            int Guess = int.Parse(txtA.Text);
+
+            //check if the user's guess right
+            if (Guess == randomNumber)
+            {
+                lblA2.Text = "You guessed it right!";
+            }
+            else
+            {
+                lblA2.Text = "oops, your guess was wrong!";
+            }
+                
+
+
+        }
+    }
+}
