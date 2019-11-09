e4b19310df2216f8e3fb6db1796cbe806c149b8d
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-11-09 10:12:47 -0500

    added and finished sum of numbers

--- FactorialW4LoopLanaZE/FactorialW4LoopLanaZE/FactorialW4LoopForm.Designer.cs
+++ FactorialW4LoopLanaZE/FactorialW4LoopLanaZE/FactorialW4LoopForm.Designer.cs
@@ -29,72 +29,79 @@
         private void InitializeComponent()
         {
             this.lblNumber = new System.Windows.Forms.Label();
-            this.txtNumber = new System.Windows.Forms.TextBox();
-            this.txtAnswer = new System.Windows.Forms.TextBox();
-            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
             this.SuspendLayout();
             // 
             // lblNumber
             // 
             this.lblNumber.AutoSize = true;
-            this.lblNumber.Font = new System.Drawing.Font("Vineta BT", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumber.Location = new System.Drawing.Point(12, 86);
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(13, 86);
+            this.lblNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNumber.Name = "lblNumber";
-            this.lblNumber.Size = new System.Drawing.Size(417, 39);
+            this.lblNumber.Size = new System.Drawing.Size(305, 42);
             this.lblNumber.TabIndex = 0;
             this.lblNumber.Text = "Enter the number";
             // 
-            // txtNumber
+            // lblSum
             // 
-            this.txtNumber.Location = new System.Drawing.Point(477, 99);
-            this.txtNumber.Name = "txtNumber";
-            this.txtNumber.Size = new System.Drawing.Size(100, 20);
-            this.txtNumber.TabIndex = 1;
-            // 
-            // txtAnswer
-            // 
-            this.txtAnswer.Location = new System.Drawing.Point(296, 219);
-            this.txtAnswer.Multiline = true;
-            this.txtAnswer.Name = "txtAnswer";
-            this.txtAnswer.Size = new System.Drawing.Size(181, 131);
-            this.txtAnswer.TabIndex = 2;
-            // 
-            // lblAnswer
-            // 
-            this.lblAnswer.AutoSize = true;
-            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAnswer.Location = new System.Drawing.Point(85, 372);
-            this.lblAnswer.Name = "lblAnswer";
-            this.lblAnswer.Size = new System.Drawing.Size(36, 25);
-            this.lblAnswer.TabIndex = 3;
-            this.lblAnswer.Text = "??";
+            this.lblSum.AutoSize = true;
+            this.lblSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSum.Location = new System.Drawing.Point(37, 514);
+            this.lblSum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(44, 31);
+            this.lblSum.TabIndex = 3;
+            this.lblSum.Text = "??";
             // 
             // btnStart
             // 
             this.btnStart.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStart.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnStart.Location = new System.Drawing.Point(296, 165);
+            this.btnStart.Location = new System.Drawing.Point(730, 92);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(181, 32);
+            this.btnStart.Size = new System.Drawing.Size(241, 76);
             this.btnStart.TabIndex = 4;
             this.btnStart.Text = "START!";
             this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
             // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 16;
+            this.lstNumbers.Location = new System.Drawing.Point(395, 191);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(288, 276);
+            this.lstNumbers.TabIndex = 5;
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(423, 106);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(187, 22);
+            this.nudNumber.TabIndex = 6;
+            // 
             // frmFactorialWLoop
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.lstNumbers);
             this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.lblAnswer);
-            this.Controls.Add(this.txtAnswer);
-            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblSum);
             this.Controls.Add(this.lblNumber);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmFactorialWLoop";
             this.Text = "Factorial with for loop by Lana ZE";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,10 +110,10 @@
         #endregion
 
         private System.Windows.Forms.Label lblNumber;
-        private System.Windows.Forms.TextBox txtNumber;
-        private System.Windows.Forms.TextBox txtAnswer;
-        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblSum;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.NumericUpDown nudNumber;
     }
 }
 
--- FactorialW4LoopLanaZE/FactorialW4LoopLanaZE/FactorialW4LoopForm.cs
+++ FactorialW4LoopLanaZE/FactorialW4LoopLanaZE/FactorialW4LoopForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿
+/*
+ * Created by: Lana ZE
+ * Created on: 09/11/2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #23 - Sum of Numbers
+ * This program adds up all the integers sequentially up to the chosen number
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +28,27 @@ namespace FactorialW4LoopLanaZE
         private void BtnStart_Click(object sender, EventArgs e)
         {
             //declare local vaiables
+            int addNumber;
+            int sum = 0;
+            int addCounter;
+
+            //no one wants to see the old numbers
+            this.lstNumbers.Items.Clear();
+
+            //get that number from the nud
+            addNumber = Convert.ToInt32(nudNumber.Value);
+
+            for (addCounter = 1; addCounter <= addNumber; addCounter++)
+            {
+                //calculate the sum by adding it by the next number
+                sum = sum + addCounter;
+
+                //add the numbers multiplied to the list
+                this.lstNumbers.Items.Add(addCounter);
+
+                //show and properly display the information
+                lblSum.Text = "All the numbers that are added sequentially to " + Convert.ToString(addNumber) + " adds up to " + Convert.ToString(sum);
+            }
         }
     }
 }
