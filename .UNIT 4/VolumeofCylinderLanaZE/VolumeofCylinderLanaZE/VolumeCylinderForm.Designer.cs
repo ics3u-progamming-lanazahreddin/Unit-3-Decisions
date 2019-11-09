d72555e312d7231fdbd50e7f12590a877440569b
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-11-09 13:00:31 -0500

    added and finished volume of a cylinder

---
+++ .UNIT 4/VolumeofCylinderLanaZE/VolumeofCylinderLanaZE/VolumeCylinderForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace VolumeofCylinderLanaZE
+{
+    partial class frmVolumeCylinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblH = new System.Windows.Forms.Label();
+            this.lblR = new System.Windows.Forms.Label();
+            this.lblVol = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblH.Location = new System.Drawing.Point(236, 75);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(65, 25);
+            this.lblH.TabIndex = 0;
+            this.lblH.Text = "height";
+            // 
+            // lblR
+            // 
+            this.lblR.AutoSize = true;
+            this.lblR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblR.Location = new System.Drawing.Point(236, 150);
+            this.lblR.Name = "lblR";
+            this.lblR.Size = new System.Drawing.Size(65, 25);
+            this.lblR.TabIndex = 1;
+            this.lblR.Text = "radius";
+            // 
+            // lblVol
+            // 
+            this.lblVol.AutoSize = true;
+            this.lblVol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVol.Location = new System.Drawing.Point(122, 366);
+            this.lblVol.Name = "lblVol";
+            this.lblVol.Size = new System.Drawing.Size(34, 25);
+            this.lblVol.TabIndex = 2;
+            this.lblVol.Text = "??";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(362, 154);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 22);
+            this.txtRadius.TabIndex = 3;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(362, 75);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 22);
+            this.txtHeight.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(241, 216);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(233, 76);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // frmVolumeCylinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblVol);
+            this.Controls.Add(this.lblR);
+            this.Controls.Add(this.lblH);
+            this.Name = "frmVolumeCylinder";
+            this.Text = "Volume of a Cylinder by Lana ZE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblH;
+        private System.Windows.Forms.Label lblR;
+        private System.Windows.Forms.Label lblVol;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ .UNIT 4/VolumeofCylinderLanaZE/VolumeofCylinderLanaZE/VolumeCylinderForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolumeofCylinderLanaZE
+{
+    public partial class frmVolumeCylinder : Form
+    {
+        public frmVolumeCylinder()
+        {
+            InitializeComponent();
+            //hide the label before the volume is calculated
+            lblVol.Hide();
+        }
+
+        //Function: Calculate Volume
+        //Input: decimal Height, decimal Radius
+        //Output: decimal
+        //Description: Calculates Volume
+        private decimal CalculateVolume(decimal height, decimal radius)
+        {
+            decimal volume;
+
+            //formula for cylindrical volume is Pi(h)(r^2)
+            volume = (decimal)Math.PI * height * radius * radius;
+
+            //round it off to 2 decimal points
+            volume = Math.Round(volume, 2);
+
+            return volume;
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            //Display the volume
+            lblVol.Text = "Volume = " + Convert.ToString
+                (CalculateVolume(decimal.Parse(txtHeight.Text), decimal.Parse(txtRadius.Text)));
+            lblVol.Show();
+        }
+    }
+}
