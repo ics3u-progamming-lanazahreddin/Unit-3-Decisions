ffb8bcd5003b5099bc81781f1b1e94f57bc68865
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-11-09 09:19:10 -0500

    added walking man and finished it

---
+++ WalkingManLanaZE/WalkingManLanaZE/WalkingManForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WalkingManLanaZE
+{
+    partial class WalkingManForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WalkingManForm));
+            this.picWalker = new System.Windows.Forms.PictureBox();
+            this.btnWalk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picWalker)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picWalker
+            // 
+            this.picWalker.Image = ((System.Drawing.Image)(resources.GetObject("picWalker.Image")));
+            this.picWalker.Location = new System.Drawing.Point(207, 32);
+            this.picWalker.Name = "picWalker";
+            this.picWalker.Size = new System.Drawing.Size(335, 383);
+            this.picWalker.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picWalker.TabIndex = 0;
+            this.picWalker.TabStop = false;
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWalk.Location = new System.Drawing.Point(21, 185);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(149, 66);
+            this.btnWalk.TabIndex = 1;
+            this.btnWalk.Text = "WALK";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.BtnWalk_Click);
+            // 
+            // WalkingManForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnWalk);
+            this.Controls.Add(this.picWalker);
+            this.Name = "WalkingManForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.WalkingManForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picWalker)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picWalker;
+        private System.Windows.Forms.Button btnWalk;
+    }
+}
+
---
+++ WalkingManLanaZE/WalkingManLanaZE/WalkingManForm.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Created by: Lana ZE
+ * Created on: 09/11/2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #21 - Walking Man
+ * This program dispplays ten images of a man walking in quick succession to make it seem as if there's a single animated image, all done upon the button being clicked
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WalkingManLanaZE
+{
+    public partial class WalkingManForm : Form
+    {
+        public WalkingManForm()
+        {
+            InitializeComponent();
+        }
+
+        //variables
+        int frameCounter = 0;
+        const int MAX_FRAMES = 10;
+
+        private void WalkingManForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnWalk_Click(object sender, EventArgs e)
+        {
+            //resets the image
+            frameCounter = 0;
+            do
+            {
+                //each time the image will change
+                frameCounter = frameCounter + 1;
+                if (frameCounter == 1)
+                {
+                    picWalker.Image = Properties.Resources.walk1;
+                }
+                else if (frameCounter == 2)
+                {
+                    picWalker.Image = Properties.Resources.walk2;
+                }
+                else if (frameCounter == 3)
+                {
+                    picWalker.Image = Properties.Resources.walk3;
+                }
+                else if (frameCounter == 4)
+                {
+                    picWalker.Image = Properties.Resources.walk4;
+                }
+                else if (frameCounter == 5)
+                {
+                    picWalker.Image = Properties.Resources.walk5;
+                }
+                else if (frameCounter == 6)
+                {
+                    picWalker.Image = Properties.Resources.walk6;
+                }
+                else if (frameCounter == 7)
+                {
+                    picWalker.Image = Properties.Resources.walk7;
+                }
+                else if (frameCounter == 8)
+                {
+                    picWalker.Image = Properties.Resources.walk8;
+                }
+                else if (frameCounter == 9)
+                {
+                    picWalker.Image = Properties.Resources.walk9;
+                }
+                else if (frameCounter == 10)
+                {
+                    picWalker.Image = Properties.Resources.walk10;
+                }
+                //refreshes the form so that the image will actually show
+                this.Refresh();
+
+                //this makes the program wait 100 milliseconds before continuing the loop, 
+                //so that the animation effect is properly shown
+                Thread.Sleep(100);
+            } while (frameCounter < MAX_FRAMES + 1);
+        }
+    }
+}
