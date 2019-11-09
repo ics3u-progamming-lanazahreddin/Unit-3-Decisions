7bb61a6658db78cf480881365a0a7d1157d9cfab
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-11-09 09:48:21 -0500

    added and finished perfect squares

---
+++ PerfectSquaresWWhileLoopLanaZE/PerfectSquaresWWhileLoopLanaZE/PerfectSquaresWWhileLoopForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PerfectSquaresWWhileLoopLanaZE
+{
+    partial class frmPSWWL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudEndValue = new System.Windows.Forms.NumericUpDown();
+            this.lstSquares = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(285, 40);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(210, 50);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // nudEndValue
+            // 
+            this.nudEndValue.Location = new System.Drawing.Point(285, 134);
+            this.nudEndValue.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudEndValue.Name = "nudEndValue";
+            this.nudEndValue.Size = new System.Drawing.Size(210, 22);
+            this.nudEndValue.TabIndex = 1;
+            this.nudEndValue.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lstSquares
+            // 
+            this.lstSquares.FormattingEnabled = true;
+            this.lstSquares.ItemHeight = 16;
+            this.lstSquares.Location = new System.Drawing.Point(285, 218);
+            this.lstSquares.Name = "lstSquares";
+            this.lstSquares.Size = new System.Drawing.Size(210, 180);
+            this.lstSquares.TabIndex = 2;
+            // 
+            // frmPSWWL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstSquares);
+            this.Controls.Add(this.nudEndValue);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmPSWWL";
+            this.Text = "Perfect Squares With While Loop by Lana ZE";
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndValue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudEndValue;
+        private System.Windows.Forms.ListBox lstSquares;
+    }
+}
+
---
+++ PerfectSquaresWWhileLoopLanaZE/PerfectSquaresWWhileLoopLanaZE/PerfectSquaresWWhileLoopForm.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by: Lana ZE
+ * Created on: 09/11/2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #22 - Perfect Squares
+ * This program that displays all the perfect squares 
+ * under a number chosen by the user
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquaresWWhileLoopLanaZE
+{
+    public partial class frmPSWWL : Form
+    {
+        public frmPSWWL()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //variables
+            int endingValue;
+            int value = 1;
+            double sqrtAsDouble;
+            int sqrtAsInt;
+
+            lstSquares.Items.Clear();
+            endingValue = Convert.ToInt32(nudEndValue.Value);
+
+            while (value <= endingValue)
+            {
+                sqrtAsDouble = Math.Sqrt(value);
+                sqrtAsInt = Convert.ToInt32(sqrtAsDouble);
+
+                if (sqrtAsInt == sqrtAsDouble)
+                {
+                    this.lstSquares.Items.Add(value + " is a perfect square");
+                    this.Refresh();
+                }
+                value = value + 1;
+            }
+
+        }
+    }
+}
