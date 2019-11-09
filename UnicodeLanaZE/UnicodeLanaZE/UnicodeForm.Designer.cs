2e217e629b8949538334a276416070639f98c39a
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-11-09 10:25:56 -0500

    added and finished unicode

---
+++ UnicodeLanaZE/UnicodeLanaZE/UnicodeForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace UnicodeLanaZE
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstUnicode = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstUnicode
+            // 
+            this.lstUnicode.FormattingEnabled = true;
+            this.lstUnicode.ItemHeight = 16;
+            this.lstUnicode.Location = new System.Drawing.Point(444, 18);
+            this.lstUnicode.Name = "lstUnicode";
+            this.lstUnicode.Size = new System.Drawing.Size(287, 420);
+            this.lstUnicode.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(64, 152);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(256, 84);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstUnicode);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode by Lana ZE";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstUnicode;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ UnicodeLanaZE/UnicodeLanaZE/UnicodeForm.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by: Lana ZE
+ * Created on: 09/11/2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #24 - Unicode
+ * This program shows all the characters from A to Z and their coresponding unicode values
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeLanaZE
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //variables
+            string character;
+            int counter;
+
+            //empty out the list beforehand
+            lstUnicode.Items.Clear();
+
+            //65 is the decimal value for A in Unicode, 
+            //90 is the decimal value for Z in Unicode 
+            for (counter = 65; counter <= 90; counter++)
+            {
+                //Convert and then add to the list the character + its unicode value
+                character = Char.ConvertFromUtf32(counter);
+                lstUnicode.Items.Add(character + " -> " + Convert.ToString(counter));
+            }
+        }
+    }
+}
