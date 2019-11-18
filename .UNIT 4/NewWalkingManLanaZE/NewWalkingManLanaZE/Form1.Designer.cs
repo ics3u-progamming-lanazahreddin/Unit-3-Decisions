e29b151343b93d3dc5985a36662aa951c87301ae
LanaZahrEddin <43419131+LanaZahrEddin@users.noreply.github.com>
2019-11-18 13:07:50 -0500

    created new walking man

---
+++ .UNIT 4/NewWalkingManLanaZE/NewWalkingManLanaZE/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace NewWalkingManLanaZE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStart.Location = new System.Drawing.Point(323, 187);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(137, 33);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(90, -5);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(607, 37);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Click on the man pictures to make it walk!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblInstructions;
+    }
+}
+
---
+++ .UNIT 4/NewWalkingManLanaZE/NewWalkingManLanaZE/Form1.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by: Lana ZE
+ * Created on: 18-november-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #32 - New Walking Man
+ * This program will change an image of a person walking to make it seem as if it was animated once you click 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewWalkingManLanaZE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //hide the instructions label
+            lblInstructions.Hide();
+        }
+
+        private void GeneratePictureBoxes()
+        {
+            GeneratePictureBox(61, 78);
+            GeneratePictureBox(464, 78);
+            GeneratePictureBox(61, 358);
+            GeneratePictureBox(61, 358);
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //display the instructions label
+            lblInstructions.Show();
+
+            //
+        }
+    }
+}
